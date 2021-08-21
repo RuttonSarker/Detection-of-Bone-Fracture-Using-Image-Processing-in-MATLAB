983c1cd693d5b73f5b64ec079aad57340fe993af
Rutton Chandra Sarker <59698649+RuttonSarker@users.noreply.github.com>
2021-08-21 22:12:41 +0600

    Startup screen added

---
+++ Bone Fracture-Image Processing/Menu.m
@@ -0,0 +1,82 @@
+function varargout = Menu(varargin)
+% MENU MATLAB code for Menu.fig
+%      MENU, by itself, creates a new MENU or raises the existing
+%      singleton*.
+%
+%      H = MENU returns the handle to a new MENU or the handle to
+%      the existing singleton*.
+%
+%      MENU('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MENU.M with the given input arguments.
+%
+%      MENU('Property','Value',...) creates a new MENU or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Menu_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Menu_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Menu
+
+% Last Modified by GUIDE v2.5 03-Aug-2021 22:08:22
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Menu_OpeningFcn, ...
+                   'gui_OutputFcn',  @Menu_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Menu is made visible.
+function Menu_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Menu (see VARARGIN)
+
+% Choose default command line output for Menu
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Menu wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Menu_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+Function
+closereq();
